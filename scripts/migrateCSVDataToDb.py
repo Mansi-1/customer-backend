656b7b6941b7e1e6e2ca5c8e45490c70155edd3c
Mansi <mansi.sharma@thoughtworks.com>
2024-04-22 04:09:33 +0530

    modify logic to handle empty list in csv file

--- scripts/migrateCSVDataToDb.py
+++ scripts/migrateCSVDataToDb.py
@@ -4,29 +4,30 @@ import requests
 
 url = "http://localhost:8080/api/v1/customer/add"
 
-with open('cust_test_1.csv', 'r') as file:
+with open('customer_data.csv', 'r') as file:
     reader = csv.DictReader(file)
 
     for row in reader:
         books = json.loads(row["books"])
+        listOfBooks = []
 
         for book in books:
-            customer_data_request_dto = {
-                "customerId": int(row["customer_id"]),
-                "customerName": row["customer_name"],
-                "listOfBooks": [
-                    {
-                        "book_id": book["book_id"],
-                        "author_name": book["author_name"],
-                        "book_name": book["book_name"],
-                        "lend_date": book["lend_date"],
-                        "days_to_return": int(book["days_to_return"])
-                    }
-                ]
-            }
+            listOfBooks.append({
+                "book_id": book["book_id"],
+                "author_name": book["author_name"],
+                "book_name": book["book_name"],
+                "lend_date": book["lend_date"],
+                "days_to_return": int(book["days_to_return"])
+            })
 
-            payload = json.dumps(customer_data_request_dto)
+        customer_data_request_dto = {
+            "customerId": int(row["customer_id"]),
+            "customerName": row["customer_name"],
+            "listOfBooks": listOfBooks
+        }
 
-            response = requests.post(url, data=payload, headers={'Content-Type': 'application/json'})
+        payload = json.dumps(customer_data_request_dto)
 
-            print(response.text)
+        response = requests.post(url, data=payload, headers={'Content-Type': 'application/json'})
+
+        print(response.text)
