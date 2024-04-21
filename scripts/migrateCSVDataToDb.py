a7901f7d73003aa6f3d27f36233ff4b722042d53
Mansi <mansi.sharma@thoughtworks.com>
2024-04-22 02:56:43 +0530

    add a script to migrate csv data to db

---
+++ scripts/migrateCSVDataToDb.py
@@ -0,0 +1,32 @@
+import csv
+import json
+import requests
+
+url = "http://localhost:8080/api/v1/customer/add"
+
+with open('cust_test_1.csv', 'r') as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        books = json.loads(row["books"])
+
+        for book in books:
+            customer_data_request_dto = {
+                "customerId": int(row["customer_id"]),
+                "customerName": row["customer_name"],
+                "listOfBooks": [
+                    {
+                        "book_id": book["book_id"],
+                        "author_name": book["author_name"],
+                        "book_name": book["book_name"],
+                        "lend_date": book["lend_date"],
+                        "days_to_return": int(book["days_to_return"])
+                    }
+                ]
+            }
+
+            payload = json.dumps(customer_data_request_dto)
+
+            response = requests.post(url, data=payload, headers={'Content-Type': 'application/json'})
+
+            print(response.text)
